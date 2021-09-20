efbf1bb310f63ff44ae911af37e09f43872c9489
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2021-09-20 13:31:53 +0200

    add vp-mode as argument to scripts

--- dns-honeypot-sensors/run.sh
+++ dns-honeypot-sensors/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mode=$1
+vp=$2
 logdir="logs"
 mkdir -p "${logdir}/${mode}"
 
-python3 -u ./forwarder.py "$mode" | tee "./${logdir}/${mode}/$(date "+%s").log"
+python3 -u ./forwarder.py "$mode" "$vp" | tee "./${logdir}/${mode}/$(date "+%s").log"
